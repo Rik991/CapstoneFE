e59c14054106ffbb3d204460fb4a34ed717ef461
Riccardo Santilli <riccardosantilli@live.it>
2025-02-19 19:47:13 +0100

    guard route fix

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -24,7 +24,7 @@ export class AuthGuard implements CanActivate, CanActivateChild {
     return this.authSvc.isLoggedIn$.pipe(
       map((isLoggedIn) => {
         if (!isLoggedIn) {
-          this.router.navigate(['/auth/login']);
+          this.router.navigate(['/']);
         }
 
         return isLoggedIn;
